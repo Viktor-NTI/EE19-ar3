c41f3ac644e060c25f51783e39d495652289026f
Viktor-NTI <viktor.larson@elev.ga.ntig.se>
2021-11-23 13:27:53 +0100

    Jobbat med morsekod och väldigt lite på en labb

---
+++ blogg/admin/admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Min enkla blogg</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/minty/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <header>
+            <h1>Sussy bakas</h1>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="nav-link" href="../index.php">Mina inlägg</a></li>
+                    <li class="nav-item"><a class="active nav-link" href="#">Admin</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            ...
+        </main>
+        <footer>
+            2021
+        </footer>
+    </div>
+</body>
+</html>
---
+++ labbar/labb-slumpa/ordsprak.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Slumpa fram sex ordspråk</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
+    <link rel="stylesheet" href="ordsprak.css">
+</head>
+<body>
+<?php
+    // Skapa en array med tio ordspråk
+    $ordsprak = [
+    "Blyga pojkar får aldrig kyssa vackra flickor.",
+    ];
+
+    // Slumpa fram ett tal mellan 0 och 9 med funktionen rand()
+    $index = rand(0, 9);
+
+    // Skriv ut ordspråket 
+    echo # kod...;
+?>
+</body>
+</html>
